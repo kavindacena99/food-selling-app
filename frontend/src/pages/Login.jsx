a1e462d584afd1d4eb9bae2b2f2f8a37f60dc1b9
kavinda <kavindach4@gmail.com>
2025-03-07 21:57:36 +0530

    feat: add login and registration pages with form handling and API integration

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../services/api";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try{
+            const { data } = await API.post("/users/login");
+            localStorage.setItem("token", data.token);
+            navigate("/dashboard");
+        }catch(err){
+            alert("Login Failed!");
+        }
+    };
+
+    return(
+        <div>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <button type="submit">Login</button>
+        </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../services/api";
+
+const Register = () => {
+    const [name, setName] = useState("");
+    
+};
