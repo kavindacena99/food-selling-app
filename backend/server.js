50bb414cc8bf86c1b679eef8d8527f3146ea8cdd
kavinda <kavindach4@gmail.com>
2025-04-12 19:09:26 +0530

    env PORT updated

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.get("/",(req,res)=>{
 
 
 // start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
